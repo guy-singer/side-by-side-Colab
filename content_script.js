295042826d3bdd377a81b8764f16b231e1c1dc39
Guy Singer <guysinger@outlook.com>
2024-01-08 11:19:20 +0200

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,5 +1,2 @@
 delay = 3; // this ensures that the colab page loads before the extension is triggered
-
-setTimeout(function(){
-    document.body.classList.add('clbflex');
-}, delay*1000);
+setTimeout(function(){document.body.classList.add('clbflex');}, delay*1000);
